bbb7304bca3945f123ae320c707e7aa389215c9a
simonec27 <164818653+simonec27@users.noreply.github.com>
2024-05-07 23:54:56 +0200

    Add files via upload

---
+++ mhw3.js
@@ -0,0 +1,261 @@
+function onCuoreClick(event) {
+    modalsection.classList.remove('hidden');
+    document.body.classList.add('noscroll');
+}
+
+const cuori = document.querySelectorAll(".cuore");
+
+cuori.forEach(cuore => {
+    cuore.addEventListener('click', onCuoreClick);
+})
+
+const login = document.querySelector('#login.black');
+login.addEventListener('click', onCuoreClick);
+
+function onExternalClick(event) {  
+    if (event.target === event.currentTarget) {
+        document.body.classList.remove('noscroll');
+    }
+    const modalsection = event.target;
+    modalsection.classList.add('hidden');
+}
+
+const sezioneesterna = document.querySelector('#modalsection');
+sezioneesterna.addEventListener('click', onExternalClick);
+
+function onXClick(event) {
+    document.body.classList.remove('noscroll'); 
+    const modalsection = document.querySelector('#modalsection');
+    modalsection.classList.add('hidden');
+}
+
+const xinterna = document.querySelector('#closebutton');
+xinterna.addEventListener('click', onXClick);
+
+function tendinaScopri(event) {
+    const altretendine=document.querySelectorAll('#tendinaAltro,#tendinaRecensioni');
+    for(let item of altretendine){
+        item.classList.add('hidden');
+    }
+    const tendina = document.querySelector('#tendinaScopri');
+    tendina.classList.remove('hidden');
+    event.stopPropagation();
+}
+
+const clickscopri=document.querySelector('#scopri');
+clickscopri.addEventListener('click', tendinaScopri);
+
+function tendinaRecensioni(event) {
+    const altretendine=document.querySelectorAll('#tendinaAltro,#tendinaScopri');
+    for(let item of altretendine){
+        item.classList.add('hidden');
+    }
+    const tendina = document.querySelector('#tendinaRecensioni');
+    tendina.classList.remove('hidden');
+    event.stopPropagation();
+}
+
+const clickrecensioni=document.querySelector('#recensioni');
+clickrecensioni.addEventListener('click', tendinaRecensioni);
+
+function tendinaAltro(event) {
+    const altretendine=document.querySelectorAll('#tendinaRecensioni,#tendinaScopri');
+    for(let item of altretendine){
+        item.classList.add('hidden');
+    }
+    const tendina = document.querySelector('#tendinaAltro');
+    tendina.classList.remove('hidden');
+    event.stopPropagation();
+}
+
+const clickaltro=document.querySelector('#altro');
+clickaltro.addEventListener('click', tendinaAltro);
+
+function onbodyclick(event){
+    const tendina=document.querySelectorAll('#tendinaAltro,#tendinaScopri,#tendinaRecensioni');
+    for(let item of tendina){
+        if(item!=event.target){
+            item.classList.add('hidden');
+        }
+    }
+}
+
+document.body.addEventListener('click',onbodyclick);
+
+function inthotel(event){
+    const int=document.createElement('h1');
+    int.textContent='Prenota un soggiorno unico';
+    int.classList.add('welcome2');
+    const container = document.querySelector('.welcome');
+    container.innerHTML = '';
+    container.appendChild(int);
+    const rc=document.querySelector('.rc');
+    rc.placeholder='Nome hotel o destinazione';
+}
+
+const hotel=document.querySelector('#hotel');
+hotel.addEventListener('click', inthotel);
+
+function intattività(event){
+    const int=document.createElement('h1');
+    int.textContent='Dedicati a qualcosa di divertente';
+    int.classList.add('welcome2');
+    const container = document.querySelector('.welcome');
+    container.innerHTML = '';
+    container.appendChild(int);
+    const rc=document.querySelector('.rc');
+    rc.placeholder='Attrazione, attività o destinazione';
+}
+
+const attività=document.querySelector('#attività');
+attività.addEventListener('click', intattività);
+
+function intristoranti(event){
+    const int=document.createElement('h1');
+    int.textContent='Trova ristoranti';
+    int.classList.add('welcome2');
+    const container = document.querySelector('.welcome');
+    container.innerHTML = '';
+    container.appendChild(int);
+    const rc=document.querySelector('.rc');
+    rc.placeholder='Ristorante o destinazione';
+}
+
+const ristoranti=document.querySelector('#ristoranti');
+ristoranti.addEventListener('click', intristoranti);
+
+function intcasevacanza(event){
+    const int=document.createElement('h1');
+    int.textContent='Scopri alloggi in locazione';
+    int.classList.add('welcome2');
+    const container = document.querySelector('.welcome');
+    container.innerHTML = '';
+    container.appendChild(int);
+    const rc=document.querySelector('.rc');
+    rc.placeholder='Località';
+}
+
+const casevacanza=document.querySelector('#casevacanza');
+casevacanza.addEventListener('click', intcasevacanza);
+
+function intcercatutto(event){
+    const int=document.createElement('h1');
+    int.textContent='Dove vuoi andare?';
+    int.classList.add('welcome2');
+    const container = document.querySelector('.welcome');
+    container.innerHTML = '';
+    container.appendChild(int);
+    const rc=document.querySelector('.rc');
+    rc.placeholder='Luoghi da visitare, cose da fare, hotel...';
+}
+
+const cercatutto=document.querySelector('#cercatutto');
+cercatutto.addEventListener('click', intcercatutto);
+
+function transition(event){
+    const newbannerimage=document.querySelectorAll('.imgbanner');
+    for(let item of newbannerimage){
+        item.src='img/borse-da-viaggio-orig.avif';
+    }
+}
+
+const bannerimage=document.querySelectorAll('.imgbanner');
+for(let item of bannerimage){
+    item.addEventListener('click', transition);
+}
+
+function bordoOn(event){
+    const celle= document.querySelectorAll('.cellericerca');
+    for(let item of celle){
+        item.classList.remove('bordocelle');
+    }
+    const selCella=event.currentTarget;
+    selCella.classList.add('bordocelle');
+    
+}
+
+const celle=document.querySelectorAll('.cellericerca');
+for (let item of celle){
+    item.addEventListener('click',bordoOn);
+}
+
+function onTkResponse(response) {
+    console.log("Risposta tk ricevuta");
+    return response.json();
+  }
+  
+  function onResponse(response) {
+    console.log('Risposta ricevuta');
+    return response.json();
+  }
+  
+  function onJson(json){
+    console.log('JSON ricevuto');
+    const library = document.querySelector('#library-view');
+    library.innerHTML = '';
+    library.classList.add('hidden');
+    let num_results = json.meta.count;
+    if(num_results > 5)
+      num_results = 5;
+  
+    for(let i=0; i<num_results; i++)
+    {
+      const data = json.data[i];
+      const nomeHotel=data.name;
+      const hotel = document.createElement('div');
+      const hotelimg=document.createElement('img');
+      hotel.classList.add('hotel');
+      hotelimg.src="img/hotel.svg";
+      hotelimg.classList.add('iconarisultato');
+      const nhcontainer=document.createElement('div');
+      nhcontainer.classList.add('nhcontainer');
+      nhcontainer.textContent=nomeHotel.toLowerCase();
+      hotel.appendChild(hotelimg);
+      hotel.appendChild(nhcontainer);
+      library.appendChild(hotel);
+      library.classList.remove('hidden');
+    }
+  }
+  
+  function onInput(event) {
+  
+    const city_input = document.querySelector('.rc');
+    const city_value = encodeURIComponent(city_input.value);
+
+    if(city_value.length==3){
+
+    function onTkJson(json) {
+      console.log("Token ricevuto: "+ json.access_token);
+      console.log('Eseguo ricerca: ' + city_value);
+      const access_token=json.access_token;
+      rest_url="https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?cityCode=" + city_value;
+      console.log('URL: ' + rest_url);
+  
+      fetch(rest_url,
+        {
+          headers:
+          {
+            'Authorization': 'Bearer ' + access_token
+          }
+        }
+      ).then(onResponse).then(onJson);
+  
+    }
+  
+    const client_id="BGK0FMrPMfksBjyC1WHcqaK6EyagghM4";
+    const client_secret="0v6E8fEC654rvAJb";
+  
+    fetch("https://test.api.amadeus.com/v1/security/oauth2/token", 
+      {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: "grant_type=client_credentials&client_id=" + client_id + "&client_secret=" + client_secret,
+      }
+    ).then(onTkResponse).then(onTkJson);
+  }
+}
+  
+  const form = document.querySelector('form');
+  form.addEventListener('input', onInput);
